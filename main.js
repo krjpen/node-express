c6766bbefa32adf3427b6149e19ae1fc08340754
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-06-27 14:18:46 +0900

    Update main.js
    add cors and express

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
 var app = http.createServer(function(request,response){
     var url = request.url;
     if(request.url == '/'){
